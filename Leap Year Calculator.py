a2b450edee72fe08ec0b7538034438f10f839f36
MRLewisR <lewisroach2002@gmail.com>
2022-02-24 11:56:28 +0000

    Seventh Commit

--- Leap Year Calculator.py
+++ Leap Year Calculator.py
@@ -3,11 +3,8 @@ carryOn = True
 while carryOn:
     year = int(input("Enter a year: "))
 
-    if (year % 4) == 0 or (year % 400) == 0:
-        if year % 100 == 0:
-            print(year, "is not a leap year \n")
-        else:
-            print(year, "is a leap year \n")
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        print(year, "is a leap year \n")
 
     else:
         print(year, "is not a leap year \n")
